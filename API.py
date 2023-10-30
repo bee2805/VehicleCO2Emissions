c3dcf0ef1a7585abba7473c87ae839f3d45f0cd6
200089_Bee! <200089@virtualwindow.co.za>
2023-10-30 13:27:35 +0200

    Found a work around - WORKING!!!
    - had to change naming conventions to match the naming convention from the model
    - the input data needed to be transformed the same way it was transformed in the prediction model.

--- API.py
+++ API.py
@@ -6,20 +6,31 @@ import joblib
 app = FastAPI()
 
 class InputModel(BaseModel):
-    EngineSizeL: float
+    EngineSize_L: float
     Cylinders: int
     Transmission: str
-    FuelType: str
-    FuelConsumptionComb: float
+    Fuel_Type: str
+    FuelConsumptionComb_L_per_100_km: float
 
-# Loading model
+# Loading model and transformer
 vehicle_emissions_model = joblib.load("vehicle_emissions_model.pkl")
+transformed_data = joblib.load("transformed_data.pkl")
 
 @app.post('/')
 async def endpoint(data: InputModel):
     try:
-        # Create a DataFrame from the input data
-        df = pd.DataFrame([data.model_dump()])
+        # Creating a DataFrame from the input data
+        df = pd.DataFrame([data.dict()])
+
+        # Rename columns to match original data
+        df = df.rename(columns={
+            'EngineSize_L': 'Engine Size(L)',
+            'Fuel_Type': 'Fuel Type',
+            'FuelConsumptionComb_L_per_100_km': 'Fuel Consumption Comb (L/100 km)'
+        })
+
+        # Apply the same transformation to the input data
+        df = transformed_data.transform(df)
 
         # Make predictions using the loaded model
         prediction = vehicle_emissions_model.predict(df)
