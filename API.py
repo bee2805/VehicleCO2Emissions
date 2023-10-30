d52fcf83a3497948f388f0ec6d5fcbfe082b3b16
200089_Bee! <200089@virtualwindow.co.za>
2023-10-30 11:23:25 +0200

    restarting with api code

--- API.py
+++ API.py
@@ -1,44 +1,16 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from uvicorn import run
 from pydantic import BaseModel
-import os
-import joblib
 
 app = FastAPI()
 
-origins = ["*"]
-methods = ["*"]
-headers = ["*"]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins = origins,
-    allow_credentials = True,
-    allow_methods = methods,
-    allow_headers = headers
-)
-
-# Loading model
-vehicle_emissions_model = joblib.load("vehicle_emissions_model.pkl")
-
-# Desfining a model to specify the expected input format
-class InputData(BaseModel):
-    Engine_Size_L: float
-    Cyliners: int
+class input_model(BaseModel):
+    EngineSizeL: float
+    Cylinders: int
     Transmission: str
-    Fuel_Type: str
-    Fuel_Consumption_Comb: int
+    FuelType: str
+    FuelConsumptionComb: float
 
-@app.post("/predict_vehicle_emissions")
-def predict_vehicle_emissions():
-    return {"message": "ENDPOINT FUNCTIONALITY PENDING"}
 
-# implementing default API endpoint
-@app.get("/") # defining url extension of endpoint
-def read_root():
-    return {"message": "Fast API setup successfully"}
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    run(app, host="0.0.0.0", port=port)
+@app.get('/')
+async def endpoint():
+    return {"message": "Endpoint set up successfully"}
