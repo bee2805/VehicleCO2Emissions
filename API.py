a57541dd55e50c3bd79d9054cc8617bccf05a39c
200089_Bee! <200089@virtualwindow.co.za>
2023-10-30 12:37:26 +0200

    making prediction but getting error:
    error: "detail": "could not convert string to float: 'AV7'" - I need to transform these values as the model expects a float due to transforming my categorical columns

--- API.py
+++ API.py
@@ -1,18 +1,31 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+import pandas as pd
 import joblib
 
 app = FastAPI()
 
-class input_model(BaseModel):
+class InputModel(BaseModel):
     EngineSizeL: float
     Cylinders: int
     Transmission: str
     FuelType: str
     FuelConsumptionComb: float
 
+# Loading model
 vehicle_emissions_model = joblib.load("vehicle_emissions_model.pkl")
 
 @app.post('/')
-async def endpoint(data: input_model):
-    return data
+async def endpoint(data: InputModel):
+    try:
+        # Create a DataFrame from the input data
+        df = pd.DataFrame([data.model_dump()])
+
+        # Make predictions using the loaded model
+        prediction = vehicle_emissions_model.predict(df)
+
+        # Return the prediction
+        return {"prediction": int(prediction)}
+    except Exception as e:
+        # Handling errors
+        raise HTTPException(status_code=500, detail=str(e))
